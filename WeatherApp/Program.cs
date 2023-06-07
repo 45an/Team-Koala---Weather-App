50fa8672b1e3728609f76f8b56b65b5d1efaa7b7
45an <muse.yusuf@hotmail.com>
2023-06-07 11:50:02 +0200

    bump

--- WeatherApp/Program.cs
+++ WeatherApp/Program.cs
@@ -4,7 +4,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args); 
+        var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
         builder.Services.AddAuthorization();

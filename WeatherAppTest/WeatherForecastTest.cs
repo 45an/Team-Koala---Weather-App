0779a0c0c388ec748700ce3f807705c4af5c6f2d
45an <114033186+45an@users.noreply.github.com>
2023-06-09 01:28:03 +0200

    Test2 (#12)
    * added unit test favorit city not founded / saved

--- WeatherAppTest/WeatherForecastTest.cs
+++ WeatherAppTest/WeatherForecastTest.cs
@@ -11,13 +11,10 @@ public class WeatherForecastTest
 
     private readonly HttpClient _httpClient = new HttpClient()
     {
-        BaseAddress = new Uri("http://dev.kjeld.io:40300")
-    };
-
-    private readonly HttpClient _httpClientTwo = new HttpClient()
-    {
         BaseAddress = new Uri("http://dev.kjeld.io:20300")
     };
+
+    
 
 
     [Fact]
@@ -36,10 +33,45 @@ public class WeatherForecastTest
     }
 
     [Fact]
+    public async Task FavoritesEndpoint_PostSavesFavoriteCity()
+    {
+        // Arrange
+        var httpClient = new HttpClient();
+        var favoriteCity = "Stockholm";
+        var httpContent = new StringContent(favoriteCity, Encoding.UTF8, "text/plain");
+
+        // Act
+        var response = await httpClient.PostAsync($"{_httpClient.BaseAddress}/favorites", httpContent);
+        var content = await response.Content.ReadAsStringAsync();
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.Equal("Favorite city saved", content);
+    }
+
+
+    [Fact]
+    public async Task FavoritesEndpoint_ReturnsNotFound()
+    {
+        // Arrange
+        var client = new HttpClient();
+
+        // Act
+        var response = await client.GetAsync($"{_httpClient}/favorites");
+        var content = await response.Content.ReadAsStringAsync();
+
+        // Assert
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        Assert.Equal("Favorite city not found", content);
+    }
+
+
+
+    [Fact]
     public async Task HealthEndpoint_ReturnsOk()
     {
         // Act
-        var response = await _httpClientTwo.GetAsync("/health");
+        var response = await _httpClient.GetAsync("/health");
         var content = await response.Content.ReadAsStringAsync();
 
         // Verify the response
